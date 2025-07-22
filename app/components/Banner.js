60bacae87dfc52f7611d5b61922d8449b218c4dc
akeliansevag <akelian.sevag@gmail.com>
2025-07-22 13:50:30 +0300

    .

---
+++ app/components/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Banner = () => {
+  return (
+    <section>
+        <div className='container'>
+            
+        </div>
+    </section>
+  )
+}
+
+export default Banner;
