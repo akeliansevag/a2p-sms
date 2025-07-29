bdf89918e2423d079865917d36fb1f6fc6fe9d5a
akeliansevag <akelian.sevag@gmail.com>
2025-07-29 12:00:26 +0300

    .

--- app/components/Messaging.js
+++ app/components/Messaging.js
@@ -9,7 +9,7 @@ const Messaging = () => {
             <div className='w-full'>
                 <h2 className='section-title !mb-0'>Let’s Talk Messaging</h2>
                 <h3 className='text-balance'>Tell us a bit about your needs and a member of our team will reach out.</h3>
-                <div className='-translate-x-10 lg:max-w-1/2 max-lg:max-w-[90%] mt-32'>
+                <div className='max-lg:-translate-x-4 max-lg:mb-10 lg:-translate-x-10 lg:max-w-1/2 max-lg:max-w-[90%] max-lg:mt-10 lg:mt-32'>
                     <img src='/emblem.webp' />
                 </div>
             </div>
